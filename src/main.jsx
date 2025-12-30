777f3a65b3d2d2b937c62cbd526133c5e19daead
nps012 <39301704+nps012@users.noreply.github.com>
2025-12-30 13:23:26 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App.jsx"
+
+const rootElement = document.getElementById("root")
+
+if (!rootElement) {
+  console.error("Root element not found")
+} else {
+  createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+}
